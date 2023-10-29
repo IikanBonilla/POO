f544f4402f3a006d046c69ba4b7b08e60da5ca97
IikanBonilla <142424897+IikanBonilla@users.noreply.github.com>
2023-10-29 17:54:48 -0500

    Update RelacionMuchosAMuchos.java

--- src/RelacionMuchosAMuchos.java
+++ src/RelacionMuchosAMuchos.java
@@ -8,7 +8,7 @@ public class RelacionMuchosAMuchos {
         //Crear objetos de Persona
         Persona persona1 = new Persona("Perisic","Masculino",1.86,34);
         Persona persona2 = new Persona("Laura","Femenino",1.70,17);
-        Persona persona3 = new Persona("Maryuri","Masculino",1.75,18);
+        Persona persona3 = new Persona("Maryuri","Femenino",1.75,18);
         Persona persona4 = new Persona("Fabian","Masculino",1.85,18);
         
         //Crear objetos de Deporte
